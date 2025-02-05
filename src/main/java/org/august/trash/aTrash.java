d575b82245c67f66e0a9e8553b89dec3abb62423
August1251 <empty>
2025-02-05 20:16:30 +0300

    feat. he project created.

---
+++ src/main/java/org/august/trash/aTrash.java
@@ -0,0 +1,16 @@
+package org.august.trash;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class aTrash extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
